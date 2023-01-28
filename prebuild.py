b4d268f27c9d9527dbe8a67fa3de8e6751023477
Yifei Kong <kong@yifei.me>
2023-01-28 10:42:35 +0800

    Fixed build bugs

--- prebuild.py
+++ prebuild.py
@@ -65,18 +65,18 @@ if uname.system != "Windows":
         f.write("from enum import IntEnum\n\n\n")
         f.write("class CurlOpt(IntEnum):\n")
         cmd = r"""
-            echo '#include "curl_cffi/include/curl/curl.h"' | gcc -E - | grep -i "CURLOPT_.\+ =" | sed "s/  CURLOPT_/\t/g" | sed "s/,//g"
+            echo '#include "curl_cffi/include/curl/curl.h"' | gcc -E - | grep -i "CURLOPT_.\+ =" | sed "s/  CURLOPT_/    /g" | sed "s/,//g"
         """
         output = subprocess.check_output(cmd, shell=True)
         f.write(output.decode())
         f.write("""
-        if locals().get("WRITEDATA"):
-            FILE = locals().get("WRITEDATA")
-        if locals().get("READDATA"):
-            INFILE = locals().get("READDATA")
-        if locals().get("HEADERDATA"):
-            WRITEHEADER = locals().get("HEADERDATA")\n\n
-    """)
+    if locals().get("WRITEDATA"):
+        FILE = locals().get("WRITEDATA")
+    if locals().get("READDATA"):
+        INFILE = locals().get("READDATA")
+    if locals().get("HEADERDATA"):
+        WRITEHEADER = locals().get("HEADERDATA")\n\n
+""")
         f.write("class CurlInfo(IntEnum):\n")
         cmd = r"""
             echo '#include "curl_cffi/include/curl/curl.h"' | gcc -E - | grep -i "CURLINFO_.\+ =" | sed "s/  CURLINFO_/\t/g" | sed "s/,//g"
@@ -84,7 +84,7 @@ if uname.system != "Windows":
         output = subprocess.check_output(cmd, shell=True)
         f.write(output.decode())
         f.write("""
-        if locals().get("RESPONSE_CODE"):
-            HTTP_CODE = locals().get("RESPONSE_CODE")\n
+    if locals().get("RESPONSE_CODE"):
+        HTTP_CODE = locals().get("RESPONSE_CODE")\n
     """)
 
