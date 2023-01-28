a3a7c6982f8e20610063d264e26c4d8305b8912c
Yifei Kong <kong@yifei.me>
2023-01-28 10:08:58 +0800

    Only run unittest when building wheels

--- tests/integration/test_fingerprints.py
+++ tests/integration/test_fingerprints.py
@@ -36,31 +36,3 @@ def test_impersonate_unknown():
         requests.get(JA3_URL, impersonate="unknown")
 
 
-#######################################################################################
-# testing httpbin
-#######################################################################################
-
-
-def test_gzip():
-    r = requests.get("https://httpbin.org/gzip")
-    assert r.status_code == 200
-    assert r.json()["gzipped"]
-
-
-def test_brotli():
-    r = requests.get("https://httpbin.org/brotli")
-    assert r.status_code == 200
-    assert r.json()["brotli"]
-
-
-def test_redirect_n():
-    r = requests.get("https://httpbin.org/absolute-redirect/3")
-    assert r.status_code == 200
-
-def test_relative_redirect_n():
-    r = requests.get("https://httpbin.org/relative-redirect/3")
-    assert r.status_code == 200
-
-def test_redirect_max():
-    with pytest.raises(requests.RequestsError, match="Failed"):
-        r = requests.get("https://httpbin.org/redirect/5", max_redirects=3)
