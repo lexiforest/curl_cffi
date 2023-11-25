c6916b828942d287b17fb19d549c732f663285f5
Yifei Kong <kong@yifei.me>
2023-11-25 11:58:18 +0800

    Change to hardcoded version numbers

--- curl_cffi/__version__.py
+++ curl_cffi/__version__.py
@@ -1,8 +1,11 @@
-from importlib import metadata
+# New in version 3.8.
+# from importlib import metadata
 from .curl import Curl
 
 
 __title__ = "curl_cffi"
-__description__ = metadata.metadata("curl_cffi")["Summary"]
-__version__ = metadata.version("curl_cffi")
+# __description__ = metadata.metadata("curl_cffi")["Summary"]
+# __version__ = metadata.version("curl_cffi")
+__description__ = "libcurl ffi bindings for Python, with impersonation support"
+__version__ = "0.5.10b5"
 __curl_version__ = Curl().version().decode()
