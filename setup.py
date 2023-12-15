49ea709a17f07218aec5274b1aad45d2fdd00c62
T-256 <132141463+T-256@users.noreply.github.com>
2023-12-16 02:56:54 +0330

    improve

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import fnmatch
+
 from setuptools import setup
+from setuptools.command.build_py import build_py
 from wheel.bdist_wheel import bdist_wheel
 
 
@@ -13,8 +16,21 @@ class bdist_wheel_abi3(bdist_wheel):
         return python, abi, plat
 
 
+excluded = ['curl_cffi/build.py']
+
+
+class excluded_build_py(build_py):
+    def find_package_modules(self, package, package_dir):
+        modules = super().find_package_modules(package, package_dir)
+        return [
+            (pkg, mod, file)
+            for (pkg, mod, file) in modules
+            if not any(fnmatch.fnmatchcase(file, pat=pattern) for pattern in excluded)
+        ]
+
+
 setup(
     # this option is only valid in setup.py
     cffi_modules=["curl_cffi/build.py:ffibuilder"],
-    cmdclass={"bdist_wheel": bdist_wheel_abi3},  # type: ignore
+    cmdclass={"bdist_wheel": bdist_wheel_abi3, 'build_py': excluded_build_py},  # type: ignore
 )
