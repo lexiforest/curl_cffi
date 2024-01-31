2f4a9f4138003ea193c772c3104eb971dc0781d7
Yifei Kong <kong@yifei.me>
2024-01-31 17:37:52 +0800

    Upgrade minimium supported version to 3.8

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ class bdist_wheel_abi3(bdist_wheel):
         python, abi, plat = super().get_tag()
 
         if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.7
-            return "cp37", "abi3", plat
+            # on CPython, our wheels are abi3 and compatible back to 3.8
+            return "cp38", "abi3", plat
 
         return python, abi, plat
 
