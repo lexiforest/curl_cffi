a2b6f98f6d8b74c93921a9480f900cebb3fa038e
Lyonnet <infinitesheldon@gmail.com>
2024-10-04 23:02:22 +0800

    Fix #395, properly update the content-type header

--- tests/unittest/test_headers.py
+++ tests/unittest/test_headers.py
@@ -1,4 +1,5 @@
 from curl_cffi.requests import Headers
+from curl_cffi.requests.session import _update_header_line
 
 
 def test_headers():
@@ -21,3 +22,15 @@ def test_header_output():
     headers = Headers({"X-Foo": "bar"})
     header_list = headers.multi_items()
     assert header_list[0][0] == "X-Foo"
+
+
+def test_replace_header():
+    header_lines = []
+    _update_header_line(header_lines, "content-type", "image/png")
+    assert header_lines == ["content-type: image/png"]
+    _update_header_line(header_lines, "Content-Type", "application/json")
+    assert header_lines == ["content-type: image/png"]
+    _update_header_line(header_lines, "Content-Type", "application/json", replace=True)
+    assert header_lines == ["Content-Type: application/json"]
+    _update_header_line(header_lines, "Host", "example.com", replace=True)
+    assert header_lines == ["Content-Type: application/json", "Host: example.com"]
