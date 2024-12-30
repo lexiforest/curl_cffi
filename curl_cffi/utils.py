c02a07639bacb1fa59f5af7f2abf611f7dae580b
Lyonnet <infinitesheldon@gmail.com>
2024-12-30 09:04:08 +0800

    Add utils.py

---
+++ curl_cffi/utils.py
@@ -0,0 +1,11 @@
+_SHOW_WARNINGS = False
+
+
+def enable_warnings():
+    global _SHOW_WARNINGS
+    _SHOW_WARNINGS = True
+
+
+def disable_warnings():
+    global _SHOW_WARNINGS
+    _SHOW_WARNINGS = False
