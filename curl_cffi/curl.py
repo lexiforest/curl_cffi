b084c0a16f8ae64c5036a2914f9ab74d1866edbe
Lyonnet <infinitesheldon@gmail.com>
2025-06-02 23:10:36 +0800

    Fix typing errors

--- curl_cffi/curl.py
+++ curl_cffi/curl.py
@@ -125,10 +125,10 @@ class Curl:
         self._resolve = ffi.NULL
         self._cacert = cacert or DEFAULT_CACERT
         self._is_cert_set = False
-        self._write_handle = None
-        self._header_handle = None
-        self._debug_handle = None
-        self._body_handle = None
+        self._write_handle: Any = None
+        self._header_handle: Any = None
+        self._debug_handle: Any = None
+        self._body_handle: Any = None
         # TODO: use CURL_ERROR_SIZE
         self._error_buffer = ffi.new("char[]", 256)
         self._debug = debug
@@ -341,10 +341,10 @@ class Curl:
     def clean_after_perform(self, clear_headers: bool = True) -> None:
         """Clean up handles and buffers after ``perform``, called at the end of
         ``perform``."""
-        self._write_handle: Any = None
-        self._header_handle: Any = None
-        self._debug_handle: Any = None
-        self._body_handle: Any = None
+        self._write_handle = None
+        self._header_handle = None
+        self._debug_handle = None
+        self._body_handle = None
         if clear_headers:
             if self._headers != ffi.NULL:
                 lib.curl_slist_free_all(self._headers)
