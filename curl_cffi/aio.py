3b8bbfd4ffae5f3608ec1dbd8f5d91628d5e0d9a
Lyonnet <infinitesheldon@gmail.com>
2025-07-18 17:28:54 +0800

    Try to streamline asyncio according to curl docs

--- curl_cffi/aio.py
+++ curl_cffi/aio.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 import warnings
 from contextlib import suppress
-from typing import Any
+from typing import Any, Optional
 from weakref import WeakKeyDictionary, WeakSet
 
 from ._wrapper import ffi, lib
@@ -84,46 +84,77 @@ CURL_CSELECT_ERR = 0x04
 CURLMSG_DONE = 1
 
 
+"""
+General notes on the implementation
+
+libcurl provides an event-based system for multiple handles with:
+
+- curl_multi_socket_action
+- curl_multi_info_read
+
+There are 2 callbacks:
+
+- socket_callback, set by CURLMOPT_SOCKETFUNCTION, will be called when socket events happen.
+- timer_callback, set by CURLMOPT_TIMERFUNCTION, will be called when timeouts happen.
+
+If there are data in/out, libcurl calls the socket_callback, and it sets up `process_data`
+as asyncio loop reader/writer function.
+
+"""
+
+
 @ffi.def_extern()
-def timer_function(curlm, timeout_ms: int, clientp: "AsyncCurl"):
+def timer_callback(curlm, timeout_ms: int, clientp: Any) -> int:
     """
     see: https://curl.se/libcurl/c/CURLMOPT_TIMERFUNCTION.html
     """
     async_curl = ffi.from_handle(clientp)
 
-    # A timeout_ms value of -1 means you should delete the timer.
-    if timeout_ms == -1:
-        for timer in async_curl._timers:
-            timer.cancel()
-        async_curl._timers = WeakSet()
-    else:
-        timer = async_curl.loop.call_later(
-            timeout_ms / 1000,
-            async_curl.process_data,
-            CURL_SOCKET_TIMEOUT,  # -1
-            CURL_POLL_NONE,  # 0
-        )
-        async_curl._timers.add(timer)
+    # Cancel the timer anyway, if it's -1, yes, libcurl says it should be cancelled.
+    # If not, to add a new timer, we need to cancel the old timer.
+    if async_curl._timer:
+        async_curl._timer.cancel()
+        async_curl._timer = None
+
+    # libcurl says to install a timer which calls socket_action on fire.
+    async_curl._timer = async_curl.loop.call_later(
+        timeout_ms / 1000,
+        async_curl.socket_action,
+        CURL_SOCKET_TIMEOUT,  # -1
+        CURL_POLL_NONE,  # 0
+    )
+
+    return 0
 
 
 @ffi.def_extern()
-def socket_function(curl, sockfd: int, what: int, clientp: "AsyncCurl", data: Any):
+def socket_callback(curl, sockfd: int, what: int, clientp: Any, data: Any) -> int:
+    """This callback is called when libcurl decides it's time to interact with certain
+    sockets"""
+
     async_curl = ffi.from_handle(clientp)
     loop = async_curl.loop
 
-    # Always remove and readd fd
+    # Always remove and re-add fds
     if sockfd in async_curl._sockfds:
         loop.remove_reader(sockfd)
         loop.remove_writer(sockfd)
 
+    # Need to read from the socket
     if what & CURL_POLL_IN:
         loop.add_reader(sockfd, async_curl.process_data, sockfd, CURL_CSELECT_IN)
         async_curl._sockfds.add(sockfd)
+
+    # Need to write to the socket
     if what & CURL_POLL_OUT:
         loop.add_writer(sockfd, async_curl.process_data, sockfd, CURL_CSELECT_OUT)
         async_curl._sockfds.add(sockfd)
-    if what & CURL_POLL_REMOVE:
+
+    # Need to remove the socket
+    if what == CURL_POLL_REMOVE:
         async_curl._sockfds.remove(sockfd)
+
+    return 0
 
 
 class AsyncCurl:
@@ -144,13 +175,13 @@ class AsyncCurl:
         self.loop = get_selector(
             loop if loop is not None else asyncio.get_running_loop()
         )
-        self._checker = self.loop.create_task(self._force_timeout())
-        self._timers: WeakSet[asyncio.TimerHandle] = WeakSet()
+        self._timeout_checker = self.loop.create_task(self._force_timeout())
+        self._timer: Optional[asyncio.TimerHandle] = None
         self._setup()
 
     def _setup(self):
-        self.setopt(CurlMOpt.TIMERFUNCTION, lib.timer_function)
-        self.setopt(CurlMOpt.SOCKETFUNCTION, lib.socket_function)
+        self.setopt(CurlMOpt.TIMERFUNCTION, lib.timer_callback)
+        self.setopt(CurlMOpt.SOCKETFUNCTION, lib.socket_callback)
         self._self_handle = ffi.new_handle(self)
         self.setopt(CurlMOpt.SOCKETDATA, self._self_handle)
         self.setopt(CurlMOpt.TIMERDATA, self._self_handle)
@@ -160,9 +191,9 @@ class AsyncCurl:
         """Close and cleanup running timers, readers, writers and handles."""
 
         # Close and wait for the force timeout checker to complete
-        self._checker.cancel()
+        self._timeout_checker.cancel()
         with suppress(asyncio.CancelledError):
-            await self._checker
+            await self._timeout_checker
 
         # Close all pending futures
         for curl, future in self._curl2future.items():
@@ -180,22 +211,20 @@ class AsyncCurl:
             self.loop.remove_writer(sockfd)
 
         # Cancel all time functions
-        for timer in self._timers:
-            timer.cancel()
+        if self._timer:
+            self._timer.cancel()
 
     async def _force_timeout(self):
         while True:
             if not self._curlm:
                 break
-            await asyncio.sleep(1)
-            # print("force timeout")
             self.socket_action(CURL_SOCKET_TIMEOUT, CURL_POLL_NONE)
+            await asyncio.sleep(1)
 
     def add_handle(self, curl: Curl):
         """Add a curl handle to be managed by curl_multi. This is the equivalent of
         `perform` in the async world."""
 
-        # import pdb; pdb.set_trace()
         curl._ensure_cacert()
         lib.curl_multi_add_handle(self._curlm, curl._curl)
         future = self.loop.create_future()
@@ -204,7 +233,8 @@ class AsyncCurl:
         return future
 
     def socket_action(self, sockfd: int, ev_bitmask: int) -> int:
-        """Call libcurl socket_action function"""
+        """wrapper for curl_multi_socket_action, 
+        returns the number of running curl handles."""
         running_handle = ffi.new("int *")
         lib.curl_multi_socket_action(self._curlm, sockfd, ev_bitmask, running_handle)
         return running_handle[0]
@@ -219,22 +249,18 @@ class AsyncCurl:
             )
             return
 
-        self.socket_action(sockfd, ev_bitmask)
-
         msg_in_queue = ffi.new("int *")
         while True:
             curl_msg = lib.curl_multi_info_read(self._curlm, msg_in_queue)
-            # print("message in queue", msg_in_queue[0], curl_msg)
+            # NULL is returned as a signal that there is no more to get at this point
             if curl_msg == ffi.NULL:
                 break
             if curl_msg.msg == CURLMSG_DONE:
-                # print("curl_message", curl_msg.msg, curl_msg.data.result)
                 curl = self._curl2curl[curl_msg.easy_handle]
                 retcode = curl_msg.data.result
                 if retcode == 0:
                     self.set_result(curl)
                 else:
-                    # import pdb; pdb.set_trace()
                     self.set_exception(curl, curl._get_error(retcode, "perform"))
             else:
                 print("NOT DONE")  # Will not reach, for no other code being defined.
