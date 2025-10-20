d498d7f73900302cc9e52500e8a87d026e67675a
Linucks <28938427+Sh3llcod3@users.noreply.github.com>
2025-10-20 22:58:08 +0100

    Continue writing second benchmark

--- benchmark/ws_bench_2.py
+++ benchmark/ws_bench_2.py
@@ -6,19 +6,15 @@ import asyncio
 import hashlib
 import mmap
 import os
-
-# import ssl
-import sys
 import time
 from argparse import ArgumentParser, Namespace
-from collections.abc import AsyncGenerator
-
-# from ssl import SSLContext
+from collections.abc import AsyncGenerator, Generator
+from secrets import compare_digest
+from ssl import PROTOCOL_TLS_SERVER, SSLContext
 from typing import cast
 
-import uvloop
-
-# from aiohttp import web
+from aiohttp import web
+from aiohttp.helpers import IS_WINDOWS
 from ws_bench_1_server import config, logger
 
 # from curl_cffi import AsyncSession, CurlOpt
@@ -28,23 +24,74 @@ TOTAL_BYTES: int = int(config.total_gb * 1024**3)
 CHUNK_SIZE: int = 4 * 1024 * 1024  # 4MiB
 
 
-async def generate_random_chunks() -> AsyncGenerator[bytes]:
-    """Generate chunks of random data.
+def compare_hash(source_hash: str, received_hash: str) -> None:
+    """Compare two hashes and log a message"""
+    if compare_digest(source_hash, received_hash):
+        logger.info("✅ Server-Side Hash Verification SUCCESSFUL")
+        return
+
+    logger.warning("Received data hash: %s", received_hash)
+    logger.warning("❌ Server-Side Hash Verification FAILED")
+
+
+def generate_random_chunks() -> Generator[bytes]:
+    """Generate chunks of random data up to a total size.
 
     Returns:
-        AsyncGenerator[bytes]: Async generator that yields random chunks.
+        Generator[bytes]: Generator that yields random chunks.
     """
     num_chunks: int = TOTAL_BYTES // CHUNK_SIZE
     for _ in range(num_chunks):
         yield os.urandom(CHUNK_SIZE)
-        await asyncio.sleep(0)
 
     remaining_size = TOTAL_BYTES % CHUNK_SIZE
     if remaining_size > 0:
         yield os.urandom(remaining_size)
 
 
-async def generate_files() -> None:
+def get_ssl_ctx() -> SSLContext | None:
+    """Load in the SSL context if cert files present and host is non-Windows.
+
+    Returns:
+        `SSLContext | None`: The SSL context or `None`.
+    """
+    if IS_WINDOWS:
+        logger.info("Disabling TLS due to Windows host")
+        return None
+
+    if not (config.cert_file.is_file() and config.cert_key.is_file()):
+        logger.warning(
+            "Certificate file(s) %s or %s not found, disabling TLS",
+            config.cert_file,
+            config.cert_key,
+        )
+        return None
+
+    ssl_context: SSLContext = SSLContext(PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(config.cert_file, config.cert_key)
+    return ssl_context
+
+
+def get_loop() -> asyncio.AbstractEventLoop:
+    """Returns the correct event loop for the platform and what's installed.
+
+    Returns:
+        asyncio.AbstractEventLoop: The created and installed event loop.
+    """
+
+    try:
+        # pylint: disable-next=import-outside-toplevel
+        import uvloop
+
+        loop: asyncio.AbstractEventLoop = uvloop.new_event_loop()
+    except ImportError:
+        loop = asyncio.new_event_loop()
+
+    asyncio.set_event_loop(loop)
+    return loop
+
+
+def generate_files() -> None:
     """
     Generate the testing data file and its hash, save it to disk.
     """
@@ -73,12 +120,11 @@ async def generate_files() -> None:
             mmap.mmap(fpath.fileno(), TOTAL_BYTES, access=mmap.ACCESS_WRITE) as mm,
         ):
             offset: int = 0
-            can_madvise = hasattr(mm, "madvise")
-            if can_madvise:
-                mm.madvise(mmap.MADV_SEQUENTIAL)
+            if hasattr(mm, "madvise"):
+                mm.madvise(mmap.MADV_SEQUENTIAL, 0, TOTAL_BYTES)
 
             logger.info("Writing data in %d chunks", TOTAL_BYTES // CHUNK_SIZE)
-            async for chunk in generate_random_chunks():
+            for chunk in generate_random_chunks():
                 hash_object.update(chunk)
                 chunk_len = len(chunk)
                 mm[offset : offset + chunk_len] = chunk
@@ -91,7 +137,7 @@ async def generate_files() -> None:
 
         # Write the hashes to file
         final_hash: str = hash_object.hexdigest()
-        _ = config.hash_filename.write_text(final_hash)
+        _ = config.hash_filename.write_text(final_hash, encoding="utf-8")
         logger.info("Hash saved to '%s': %s", config.hash_filename, final_hash)
 
         if elapsed_time > 0:
@@ -155,7 +201,103 @@ async def stream_test_data() -> AsyncGenerator[bytes]:
         )
 
 
-async def main() -> None:
+async def server_recv_handler(ws: web.WebSocketResponse) -> None:
+    """Handle the server side download benchmark.
+
+    Args:
+        ws (web.WebSocketResponse): WebSocket object.
+    """
+    logger.info("Client connected for UPLOAD")
+    if not (config.hash_filename.is_file() and config.hash_filename.stat().st_size):
+        logger.error("Hash file '%s' not found or empty", config.hash_filename)
+        return
+
+    source_hash: str = config.hash_filename.read_text("utf-8").strip()
+    logger.info("Loaded hash for '%s': %s", config.data_filename, source_hash)
+
+    # Allocate data buffer ahead of time
+    logger.info("Allocating memory buffer of %d GB", config.total_gb)
+    testdata_buffer: bytearray = bytearray()
+    try:
+        start_time: float = time.perf_counter()
+        testdata_buffer = bytearray(TOTAL_BYTES)
+        elapsed_time: float = time.perf_counter() - start_time
+        logger.info(
+            "Allocated %d GB in %.2fs. Speed: %.2f MiB/s ",
+            config.total_gb,
+            elapsed_time,
+            (TOTAL_BYTES / (1024**2)) / elapsed_time,
+        )
+    except MemoryError:
+        logger.exception("Not enough RAM to allocate %d GB", config.total_gb)
+        return
+
+    try:
+        # Receive the data into the buffer
+        offset: int = 0
+        logger.info("Receiving data from client")
+        start_time = time.perf_counter()
+        async for msg in ws:
+            if msg.type == web.WSMsgType.BINARY:
+                msg_len: int = len(msg.data)
+                testdata_buffer[offset : offset + msg_len] = msg.data
+                offset += msg_len
+
+        elapsed_time = time.perf_counter() - start_time
+        logger.info(
+            "Received %.2f GB in %.2fs. Speed: %.2f MiB/s ",
+            offset / (1024**3),
+            elapsed_time,
+            (offset / (1024**2)) / elapsed_time,
+        )
+
+        # Calculate and compare hashes
+        logger.info("Calculating hash of received data")
+        start_time = time.perf_counter()
+        received_hash = hashlib.sha256(testdata_buffer).hexdigest()
+        elapsed_time = time.perf_counter() - start_time
+        logger.info(
+            "Hash calculated in %.2fs. Speed: %.2f MiB/s ",
+            elapsed_time,
+            (offset / (1024**2)) / elapsed_time,
+        )
+        compare_hash(source_hash, received_hash)
+    except Exception:
+        logger.exception("Failed to receive data from client")
+
+    del testdata_buffer
+
+
+async def server_send_handler(ws: web.WebSocketResponse) -> None:
+    """Handle the server side upload benchmark.
+
+    Args:
+        ws (web.WebSocketResponse): WebSocket object.
+    """
+
+
+async def ws_handler(request: web.Request) -> web.WebSocketResponse:
+    """Handle the request and run the relevant server action.
+
+    Args:
+        request (web.Request): HTTP request object.
+
+    Returns:
+        web.WebSocketResponse: Response object.
+    """
+    ws: web.WebSocketResponse = web.WebSocketResponse()
+    _ = await ws.prepare(request)
+    logger.info("Client connected %s", request.host)
+
+    if request.query.get("test") == "upload":
+        await server_recv_handler(ws)
+    else:
+        await server_send_handler(ws)
+
+    return ws
+
+
+def main() -> None:
     """Entrypoint"""
     parser: ArgumentParser = ArgumentParser(
         description="WebSocket Unidirectional Benchmark"
@@ -171,11 +313,23 @@ async def main() -> None:
     args.test = cast(str, args.test)
 
     if args.mode == "generate":
-        await generate_files()
+        generate_files()
 
     elif args.mode == "server":
-        ...
-        # await start_server()
+        app: web.Application = web.Application(logger=logger)
+        _ = app.add_routes(routes=[web.get("/ws", ws_handler)])
+        logger.info(
+            "Starting server on wss://%s:%d/ws", config.srv_host, config.srv_port
+        )
+        web.run_app(
+            app,
+            host=config.srv_host.exploded,
+            port=config.srv_port,
+            loop=get_loop(),
+            ssl_context=get_ssl_ctx(),
+            access_log=logger,
+            print=logger.debug,
+        )
 
     # elif args.mode == "client":
     #     if args.test == "download":
@@ -192,7 +346,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    if sys.platform != "win32":
-        uvloop.run(main())
-    else:
-        asyncio.run(main())
+    main()
