27dd9e572993bf7fd9ea7bb81b9edcce1615eec5
Lyonnet <infinitesheldon@gmail.com>
2026-02-02 11:54:04 +0800

    Try 1.4.2

--- scripts/build.py
+++ scripts/build.py
@@ -12,7 +12,7 @@ from urllib.request import urlretrieve
 from cffi import FFI
 
 # this is the upstream libcurl-impersonate version
-__version__ = "1.4.1"
+__version__ = "1.4.2"
 
 
 def detect_arch():
