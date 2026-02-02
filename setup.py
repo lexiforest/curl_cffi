159963ae373fe4da6addedbe239ab7cfebf0dc51
Lyonnet <infinitesheldon@gmail.com>
2026-02-02 15:24:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ class bdist_wheel_abi3(bdist_wheel):
     def get_tag(self):
         python, abi, plat = super().get_tag()
 
-        if "android" in plat:
-            if python.startswith("cp") and not (python.endswith("t") or abi.endswith("t")):
-                return python, "abi3", plat
-            return python, abi, plat
-
         if python.startswith("cp") and not (python.endswith("t") or abi.endswith("t")):
+            if "android" in plat:
+                # cibuildwheel supports android since cp313, so we can't mark it as 310
+                return python, "abi3", plat
             # On CPython, our wheels are abi3 and compatible back to 3.10.
             # Free-threaded builds ("t" tag) must keep their original tags (PEP 803).
             # Once PEP 803 is accepted, we may be able to build abi3t wheels.
