25cb02a87f9d4193a3cd23eb781f2cf83cd8b1ed
lexiforest <infinitesheldon@gmail.com>
2026-06-13 12:33:09 +0800

    Bugfix: pass cacert to async curl handles

--- curl_cffi/requests/utils.py
+++ curl_cffi/requests/utils.py
@@ -838,10 +838,12 @@ def set_curl_options(
     # cert for this single request
     if isinstance(verify, str):
         c.setopt(CurlOpt.CAINFO, verify)
+        c.setopt(CurlOpt.PROXY_CAINFO, verify)
 
     # cert for the session
     if verify in (None, True) and isinstance(base_verify, str):
         c.setopt(CurlOpt.CAINFO, base_verify)
+        c.setopt(CurlOpt.PROXY_CAINFO, base_verify)
 
     # referer
     if referer:
